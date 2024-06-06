3baeaa5b476d199089075657098b32146a9c552e
Lils06 <131480845+Lils06@users.noreply.github.com>
2024-06-07 01:06:17 +0800

    Update Module2.sol

--- Module2.sol
+++ Module2.sol
@@ -14,6 +14,6 @@ contract DailyFoodConsumption {
     }
 
     function Dinner(uint256 DinnerConsumpt) public pure {
-        assert(DinnerConsumpt == 20);
+        assert(DinnerConsumpt == 20, "I'm still hungry, I need food in my dinner.");
     }
 }
