e7a92910a393ec152a2e94446e1793d5e71af776
Lils06 <131480845+Lils06@users.noreply.github.com>
2024-06-10 03:39:27 +0800

    Update Module2.sol

--- Module2.sol
+++ Module2.sol
@@ -1,19 +1,19 @@
-/ SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
 contract DailyFoodConsumption {
 
     function Morning(uint MorningConsumpt) public pure {
-        require(MorningConsumpt > 100, "Im still hungry, I need more food in my breakfast.");
+        require(MorningConsumpt > 100, "I'm still hungry, I need more food in my breakfast.");
     }
 
     function Lunch(uint LunchConsumpt) public pure {
         if (LunchConsumpt < 50) {
-            revert("Im still hungry, I need food in my lunch.");
+            revert("I'm still hungry, I need food in my lunch.");
         }
     }
 
     function Dinner(uint256 DinnerConsumpt) public pure {
-        assert(DinnerConsumpt >= 20, "I'm still hungry, I need food in my dinner.");
+        require(DinnerConsumpt >= 20, "I'm still hungry, I need more food in my Dinner.");
     }
 }
